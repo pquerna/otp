4a83670d01f461636451739d87163d888dc55b30
Ryan Maleki <spring1843@users.noreply.github.com>
2021-08-17 14:51:41 -0700

    Fix typo (#65)
    s/paramters/parameters

--- example/main.go
+++ example/main.go
@@ -33,7 +33,7 @@ func promptForPasscode() string {
 }
 
 // Demo function, not used in main
-// Generates Passcode using a UTF-8 (not base32) secret and custom paramters
+// Generates Passcode using a UTF-8 (not base32) secret and custom parameters
 func GeneratePassCode(utf8string string) string{
         secret := base32.StdEncoding.EncodeToString([]byte(utf8string))
         passcode, err := totp.GenerateCodeCustom(secret, time.Now(), totp.ValidateOpts{
