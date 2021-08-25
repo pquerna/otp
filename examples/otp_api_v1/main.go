4aa7a19e85910d1ad6a61fe447116b8bcca0609a
Davood Falahati <falahati.davood@gmail.com>
2021-08-26 04:08:53 +0430

    tests are ok

--- examples/otp_api_v1/main.go
+++ examples/otp_api_v1/main.go
@@ -33,8 +33,7 @@ func promptForPasscode() string {
 }
 
 // Demo function, not used in main
-<<<<<<< HEAD:examples/otp_api_v1/main.go
-// Generates Passcode using a UTF-8 (not base32) secret and custom paramters
+// Generates Passcode using a UTF-8 (not base32) secret and custom parameters
 func GeneratePassCode(utf8string string) string {
 	secret := base32.StdEncoding.EncodeToString([]byte(utf8string))
 	passcode, err := totp.GenerateCodeCustom(secret, time.Now(), totp.ValidateOpts{
@@ -47,21 +46,6 @@ func GeneratePassCode(utf8string string) string {
 		panic(err)
 	}
 	return passcode
-=======
-// Generates Passcode using a UTF-8 (not base32) secret and custom parameters
-func GeneratePassCode(utf8string string) string{
-        secret := base32.StdEncoding.EncodeToString([]byte(utf8string))
-        passcode, err := totp.GenerateCodeCustom(secret, time.Now(), totp.ValidateOpts{
-                Period:    30,
-                Skew:      1,
-                Digits:    otp.DigitsSix,
-                Algorithm: otp.AlgorithmSHA512,
-        })
-        if err != nil {
-                panic(err)
-        }
-        return passcode
->>>>>>> 4a83670d01f461636451739d87163d888dc55b30:example/main.go
 }
 
 func main() {
