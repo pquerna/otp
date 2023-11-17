44a4287cb99b13a1c52ea57db0407087f4562218
Erfan Khadem <45465346+er888kh@users.noreply.github.com>
2023-11-17 17:59:45 +0330

    Replace deprecated ioutil

--- example/main.go
+++ example/main.go
@@ -9,7 +9,6 @@ import (
 	"encoding/base32"
 	"fmt"
 	"image/png"
-	"io/ioutil"
 	"os"
 	"time"
 )
@@ -19,7 +18,7 @@ func display(key *otp.Key, data []byte) {
 	fmt.Printf("Account Name: %s\n", key.AccountName())
 	fmt.Printf("Secret:       %s\n", key.Secret())
 	fmt.Println("Writing PNG to qr-code.png....")
-	ioutil.WriteFile("qr-code.png", data, 0644)
+	os.WriteFile("qr-code.png", data, 0644)
 	fmt.Println("")
 	fmt.Println("Please add your TOTP to your OTP Application now!")
 	fmt.Println("")
